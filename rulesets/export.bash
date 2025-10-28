22b311b844ce3b59d0cf16286c61e0cebc5671da
Wolfgang Walther <walther@technowledgy.de>
2025-10-28 09:59:15 +0100

    rulesets/export: remove timestamp
    No need to track timestamps - we have the git log for that.

--- rulesets/export.bash
+++ rulesets/export.bash
@@ -7,4 +7,4 @@ REPO="${1:?Please pass the repository name as the only argument.}"
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
 gh api "/repos/NixOS/$REPO/rulesets" --jq '.[] | select(.source_type == "Repository") | "\(._links.self.href) \(.name)"' \
-	| xargs -n2 sh -c "gh api \$0 | jq 'del(.node_id, ._links)' > $REPO/\$1.json"
+	| xargs -n2 sh -c "gh api \$0 | jq 'del(.node_id, ._links, .created_at, .updated_at)' > $REPO/\$1.json"
